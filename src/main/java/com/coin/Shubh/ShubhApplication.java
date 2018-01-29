a41407975911b73825efb7acbb22d08117657e70
Shubham Bhindwal <shubhambhindwal123@gmail.com>
2018-01-29 19:46:15 +0530

    first commit

---
+++ src/main/java/com/coin/Shubh/ShubhApplication.java
@@ -0,0 +1,12 @@
+package com.coin.Shubh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShubhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShubhApplication.class, args);
+	}
+}
